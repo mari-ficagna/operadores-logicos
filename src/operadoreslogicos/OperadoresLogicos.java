182f6435a3d4cce3ac818bbfb5b74a5732ef29f9
Marieli Ficagna <marieli.ficagna@gmail.com>
2023-02-15 17:30:30 -0300

    first commit

---
+++ src/operadoreslogicos/OperadoresLogicos.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package operadoreslogicos;
+
+/**
+ *
+ * @author Mari (48) 99618-5728
+ */
+public class OperadoresLogicos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x,y,z;
+        x = 4;
+        y = 7;
+        z = 4;
+        boolean r;
+        r = (x<y && y<z) ? true:false;
+        System.out.println(r);
+    }
+    
+}
